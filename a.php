3aab5b09369917c2223178b64aa23cae47c7ec10
KATA_KUNA <alexandru.azoicai@webmagnat.ro>
2014-01-09 13:23:20 +0200

    ultima
    ultima

--- a.php
+++ a.php
@@ -2,22 +2,13 @@
 
 
 echo "test testulet";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 //ultima faza
 
-=======
-
-=======
-
->>>>>>> 358b5bd6eb33088906b9c0f328f90886a953a7ad
 
 //inca ce
 ///ouiouio
 
-<<<<<<< HEAD
->>>>>>> 358b5bd6eb33088906b9c0f328f90886a953a7ad
-=======
->>>>>>> 358b5bd6eb33088906b9c0f328f90886a953a7ad
+
 echo "testulet testisor"; 
 ?>
